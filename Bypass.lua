6eede2055c3030c367d005f05170a570beac1d2a
GetRioToday <riosoftware@proton.me>
2026-05-13 10:08:04 -0400

    Update Bypass.lua

--- Bypass.lua
+++ Bypass.lua
@@ -1,29 +1,29 @@
 local function BypassAdonis()
 	local Query = {
 		--Hash = "605A34235509ABBB6946D10B499B526AC72774E441C3F9BAE4B63D321C6E759347D864CE879B39864743C0EEAC444FFF";
-		Constants = {" - On Xbox", " - On mobile", "_"};
-		IgnoreExecutor = true;
+		Constants = {" - On Xbox", " - On mobile", "_"}
+		IgnoreExecutor = true
 	}
 
-	local Detected = filtergc("function", Query); Detected = Detected and Detected[1];
+	local Detected = filtergc("function", Query, true)
 	if not Detected then
-		--Telemetry:Log("BypassAntiCheat.BypassAdonis", LPH_LINE, "Failed to locate \"Detected\" function from Adonis Anti module.")
-		return LocalClient:Kick("Anticheat bypass failed to activate. This can be the result of an anticheat update. Please contact support.")
+		return LocalClient:Kick("Adonis anticheat bypass failed to activate, please contact support.")
 	end
 
-	local iDetour = Hooks.Create(REnv.debug.info)
-	local dDetour = Hooks.Create(Detected)
+	local iDetour = Hooks.new(REnv.debug.info)
+	local dDetour = Hooks.new(Detected)
 
-	local a, b, c, d, e, f = REnv.debug.info(Detected, "slanf")
-	local Jump = function(...)
+	local a, b, c, d, e, f = debug.info(Detected, "slanf")
+	local Detour = function(...)
 		local Args = {...}
 		if Args[1] == Detected and Args[2] == "slanf" then
-			return a, b, c, d, e, f end;
+			return a, b, c, d, e, f
+		end
 
 		return iDetour:Original(...)
 	end
 
-	iDetour:SetDetour(Jump)
+	iDetour:SetDetour(Detour)
 	dDetour:SetDetour(function() return task.wait(9e10) end)
 	iDetour:Enable()
 	dDetour:Enable()
